b65ad0d49aead38de3d1ce70e5fd061bd272d93e
Maísa Lima <maisafernanda@hotmail.com>
2023-01-13 13:23:16 -0300

    criacao de pastas e arquivos de validacao

--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -1,14 +1,15 @@
-import express from 'express';
-import CompanyController from '../controllers/company.controller';
+import express from "express";
+import CompanyController from "../controllers/company.controller";
+import companyCreateValidation from "../middlewares/validations/company/create";
 
 const routes = express.Router();
 
 const companyController = new CompanyController();
 
-routes.get('/', companyController.index);
-routes.get('/:id', companyController.show);
-routes.post('/', companyController.create);
-routes.put('/:id', companyController.update);
-routes.delete('/:id', companyController.delete);
+routes.get("/", companyController.index);
+routes.get("/:id", companyController.show);
+routes.post("/", companyCreateValidation, companyController.create);
+routes.put("/:id", companyController.update);
+routes.delete("/:id", companyController.delete);
 
 export default routes;
