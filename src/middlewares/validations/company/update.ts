f35102f67c38e437ae541e5c9b1e6983dcef0258
Maísa Lima <maisafernanda@hotmail.com>
2023-01-13 13:46:57 -0300

    validacao update company

---
+++ src/middlewares/validations/company/update.ts
@@ -0,0 +1,25 @@
+import { validate, Joi } from "express-validation";
+
+export = validate({
+  params: Joi.object({
+    id: Joi.string().required(),
+  }),
+  body: Joi.object({
+    companyName: Joi.string().required(),
+    site: Joi.string(),
+    responsibleName: Joi.string().required(),
+    responsiblePhone: Joi.number(),
+    companyEmail: Joi.string().email().required(),
+    address: {
+      street: Joi.string().required(),
+      zip: Joi.string().required(),
+      city: Joi.string().required(),
+      state: Joi.string().required(),
+    },
+    phone: Joi.number().required(),
+    typesOfMaterialYouRecycle: Joi.string(),
+    removeTheMaterialAtAnotherAddress: Joi.string(),
+    loginEmail: Joi.string().email().required(),
+    password: Joi.string().min(6).required,
+  }),
+});
--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import CompanyController from "../controllers/company.controller";
 import companyCreateValidation from "../middlewares/validations/company/create";
 import companyDeleteValidation from "../middlewares/validations/company/delete";
+import companyUpdateValidation from "../middlewares/validations/company/update";
 
 const routes = express.Router();
 
@@ -10,7 +11,7 @@ const companyController = new CompanyController();
 routes.get("/", companyController.index);
 routes.get("/:id", companyController.show);
 routes.post("/", companyCreateValidation, companyController.create);
-routes.put("/:id", companyController.update);
+routes.put("/:id", companyUpdateValidation, companyController.update);
 routes.delete("/:id", companyDeleteValidation, companyController.delete);
 
 export default routes;
