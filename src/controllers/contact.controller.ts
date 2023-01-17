cbb96f57a109f0a462c6e424019c526ceb1492b8
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2023-01-16 23:39:55 -0300

    Criando aviso de email enviado

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -1,42 +1,36 @@
 import { Request, Response } from "express";
 import prisma from "../database";
+import mailOptions, { transport } from "../modules/mailer";
 
 class ContactController {
-    public async index(req: Request, res: Response) {
-      const contacts = await prisma.company.findMany();
-  
-      res
-        .json({ message: "Mensagem enviada com sucesso!", contacts })
-        .status(200);
-    }
+  public async create(req: Request, res: Response) {
+    const { firstName, lastName, email, message } = req.body;
 
-
-    public async show(req: Request, res: Response) {
-        const { id } = req.params;
-    
-        const contacts = await prisma.user.findFirst({
-          where: {
-            id,
-          },
-        });
-
-        if (!contacts) {
-            return res.status(404).json({ error: "Erro ao enviar sua mensagem!" });
-          }
-      
-          return res.json(contacts).status(200);
+    try {
+      const options = mailOptions(
+        ["contato@recollect.com.br"],
+        `Recebemos um contato de ${firstName} ${lastName}`,
+        "contact",
+        {
+          firstName,
+          lastName,
+          email,
+          message,
         }
+      );
 
-     public async create(req: Request, res: Response) {
-    const {
-      FirtName,
-      LastName,
-      email,
-      message,
-      } = req.body;
+      transport.sendMail(options, (error: any) => {
+        if (error) {
+          return res
+            .status(400)
+            .send("Falha no envio do email, tente novamente.");
+        }
+        return res.status(201).send("Email enviado com sucesso.");
+      });
+    } catch (error) {
+      res.status(500).send("Ocorreu algum problema, contate o suporte.");
     }
-
-    
+  }
 }
 
 export default ContactController;
