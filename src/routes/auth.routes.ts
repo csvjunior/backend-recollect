bf4cad253bb62e27ffcf196f652a3a916313c8f6
Maísa Lima <maisafernanda@hotmail.com>
2023-01-20 14:51:56 -0300

    alteração em auth.routes.ts

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,7 +2,6 @@ import express from "express";
 import AuthController from "../controllers/auth.controller";
 import authLoginValidation from "../middlewares/validations/auth/login";
 import authForgotPasswordValidation from "../middlewares/validations/auth/forgot.password";
-import authResetPassword from "../middlewares/validations/auth/reset.password";
 
 const routes = express.Router();
 
